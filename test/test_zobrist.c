938578e9755aed9ca2ffc8f085e258f29cb86494
PavolUlicny <pavolko.u@gmail.com>
2026-02-15 12:20:08 +0100

    test(zobrist): add seed boundaries and toggle symmetry tests

--- test/test_zobrist.c
+++ test/test_zobrist.c
@@ -62,9 +62,56 @@ void test_same_position_same_hash(void)
     TEST_ASSERT_EQUAL_UINT64(hash1, hash2);
 }
 
+// Test zobrist seed boundary values
+void test_zobrist_seed_boundaries(void)
+{
+    Bitboard board = {0, 0};
+    bitboard_make_move(&board, 0, 0, 'x');
+
+    // Test seed 0
+    zobrist_set_seed(0);
+    zobrist_init();
+    uint64_t hash_seed0 = zobrist_hash(board, 'x');
+
+    // Test seed UINT64_MAX
+    zobrist_set_seed(UINT64_MAX);
+    zobrist_init();
+    uint64_t hash_seedmax = zobrist_hash(board, 'x');
+
+    // Different seeds should produce different hashes
+    TEST_ASSERT_NOT_EQUAL_UINT64(hash_seed0, hash_seedmax);
+
+    // Both hashes should be non-zero
+    TEST_ASSERT_NOT_EQUAL_UINT64(0, hash_seed0);
+    TEST_ASSERT_NOT_EQUAL_UINT64(0, hash_seedmax);
+}
+
+// Test zobrist toggle symmetry (repeated toggles)
+void test_zobrist_toggle_symmetry(void)
+{
+    zobrist_set_seed(42);
+    zobrist_init();
+
+    Bitboard board = {0, 0};
+    uint64_t hash = zobrist_hash(board, 'x');
+    uint64_t original_hash = hash;
+
+    // Toggle same cell 10 times (5 pairs)
+    for (int i = 0; i < 5; i++)
+    {
+        hash = zobrist_toggle(hash, 1, 1, 'x');
+        hash = zobrist_toggle(hash, 1, 1, 'x');
+    }
+
+    // Should return to original (XOR symmetry)
+    TEST_ASSERT_EQUAL_UINT64(original_hash, hash);
+}
+
 void test_zobrist_suite(void)
 {
     RUN_TEST(test_incremental_hash_matches_full);
     RUN_TEST(test_turn_toggle_changes_hash);
     RUN_TEST(test_same_position_same_hash);
+    RUN_TEST(test_zobrist_seed_boundaries);
+    RUN_TEST(test_zobrist_toggle_symmetry);
 }
