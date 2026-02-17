2385d1fb36ff587be7e24d0bad64d1d17f533e89
PavolUlicny <pavolko.u@gmail.com>
2026-02-17 19:35:07 +0100

    refactor(minimax): use shared bitops.h for CTZ64 definition

--- src/MiniMax/mini_max.c
+++ src/MiniMax/mini_max.c
@@ -13,22 +13,8 @@
 
 #include "mini_max.h"
 #include "transposition.h"
+#include "bitops.h"
 #include <stdint.h>
-
-/* Portable count-trailing-zeros for 64-bit integers */
-#if defined(__GNUC__) || defined(__clang__)
-#define HAS_CTZ64
-#define CTZ64(x) __builtin_ctzll(x)
-#elif defined(_MSC_VER) && defined(_WIN64)
-#include <intrin.h>
-#define HAS_CTZ64
-static inline int CTZ64(uint64_t x)
-{
-    unsigned long index;
-    _BitScanForward64(&index, x);
-    return (int)index;
-}
-#endif
 
 /* A single board coordinate (row, col). */
 typedef struct
--- src/MiniMax/transposition.c
+++ src/MiniMax/transposition.c
@@ -5,23 +5,9 @@
  */
 
 #include "transposition.h"
+#include "bitops.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-/* Portable count-trailing-zeros for 64-bit integers */
-#if defined(__GNUC__) || defined(__clang__)
-#define HAS_CTZ64
-#define CTZ64(x) __builtin_ctzll(x)
-#elif defined(_MSC_VER) && defined(_WIN64)
-#include <intrin.h>
-#define HAS_CTZ64
-static inline int CTZ64(uint64_t x)
-{
-    unsigned long index;
-    _BitScanForward64(&index, x);
-    return (int)index;
-}
-#endif
 
 /*
  * Zobrist keys: [row][col][player_index]
