e6b13618afe220737fe039824076ebd66cf38d65
PavolUlicny <pavolko.u@gmail.com>
2026-02-25 17:57:52 +0100

    chore(bitops): remove unused HAS_CTZ64 macro

--- src/Negamax/bitops.h
+++ src/Negamax/bitops.h
@@ -5,11 +5,9 @@
 
 /* Portable count-trailing-zeros for 64-bit integers */
 #if defined(__GNUC__) || defined(__clang__)
-#define HAS_CTZ64
 #define CTZ64(x) __builtin_ctzll(x)
 #elif defined(_MSC_VER) && defined(_WIN64)
 #include <intrin.h>
-#define HAS_CTZ64
 static inline int CTZ64(uint64_t x)
 {
     unsigned long index;
